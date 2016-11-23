5db32e5d1a64119c5a2527d516933d98be0d2af4
Jeffrey <anglyeds@gmail.com>
2016-11-23 14:25:11 +0800

    add script and image folder

--- system/resources/views/layouts/app.blade.php
+++ system/resources/views/layouts/app.blade.php
@@ -187,8 +187,8 @@
     <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
 
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.min.js"></script>
-    <script src="system/resources/views/scrollfix.js" type="text/javascript"></script>
-    <script type="text/javascript" src="system/resources/views/script.js"></script>       
+    <script src="system/resources/views/script/scrollfix.js" type="text/javascript"></script>
+    <script type="text/javascript" src="system/resources/views/script/script.js"></script>       
     
     <!-- <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
